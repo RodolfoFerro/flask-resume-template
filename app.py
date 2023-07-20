b4ae219cd0e72cc2f9c5208ad479a3d52d12d577
imkevinliao <87458144+imkevinliao@users.noreply.github.com>
2023-07-20 22:44:43 +0800

    Update app.py
    Now yaml not use  yaml.load(file), but yaml.load(file,Loader = yaml.FullLoader)

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.config['JSON_SORT_KEYS'] = False
 
 @app.route('/')
 def index():
-    website_data = yaml.load(open('_config.yaml'))
+    website_data = yaml.load(open('_config.yaml'), Loader=yaml.FullLoader)
 
     return render_template('index.html', data=website_data)
 
